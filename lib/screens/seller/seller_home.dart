3f37a0ce6c10082e9c4141a5e99f92c2ca34c15f
Manan Soni <96214089+Manan-Soni@users.noreply.github.com>
2026-02-28 08:56:21 +0530

    Refactor HomeScreen to SellerHome with state management

---
+++ lib/screens/seller/seller_home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  @override
+  State<SellerHome> createState() => _SellerHomeState();
+}
